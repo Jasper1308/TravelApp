fc865556d6d4dc6133a56515b7ec944a17b76ca8
Jasper1308 <adrianpjasper@gmail.com>
2025-06-27 00:41:54 -0300

    Implementing toString extension for enum

--- lib/models/enums/experience_type.dart
+++ lib/models/enums/experience_type.dart
@@ -1,7 +1,18 @@
-enum ExperienceType{
-  culture,
-  culinary,
-  historic,
-  localEstablishments,
-  nature
+enum ExperienceType { culture, culinary, historic, localEstablishments, nature }
+
+extension ExperienceTypeExtension on ExperienceType {
+  String get toString {
+    switch (this) {
+      case ExperienceType.culture:
+        return 'Cultura';
+      case ExperienceType.culinary:
+        return 'Culinaridade';
+      case ExperienceType.historic:
+        return 'História';
+      case ExperienceType.localEstablishments:
+        return 'Estabelecimentos Locais';
+      case ExperienceType.nature:
+        return 'Natureza';
+    }
+  }
 }
