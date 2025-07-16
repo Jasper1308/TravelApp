a97d9b9d5883fdf3b9ed5c15d797a6fbdc49ed6b
Jasper1308 <adrianpjasper@gmail.com>
2025-07-16 16:41:35 -0300

    feat: created load and setTheme functions on preferences class

--- lib/controllers/app_preferences_provider.dart
+++ lib/controllers/app_preferences_provider.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppPreferencesState extends ChangeNotifier {
+  static const String _themeModeKey = 'themeMode';
+  ThemeMode _themeMode = ThemeMode.system;
 
-}
+  ThemeMode get themeMode => _themeMode;
 
-class ThemePreference {
-  static ValueNotifier<Brightness> theme = ValueNotifier(Brightness.light);
+  AppPreferencesState() {
+    _loadPreferences();
+  }
 
-  static setTheme() {
-    theme.value = WidgetsBinding.instance.platformDispatcher.platformBrightness;
+  Future<void> _loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final int? themeModeValue = prefs.getInt(_themeModeKey);
+    if (themeModeValue != null) {
+      _themeMode = ThemeMode.values[themeModeValue];
+    } else {
+      _themeMode = ThemeMode.system;
+    }
+    notifyListeners();
+  }
+
+  void setThemeMode(ThemeMode themeMode) {
+    if (_themeMode == themeMode) return;
+    _themeMode = themeMode;
+    _saveThemeMode();
+    notifyListeners();
+  }
+
+  void _saveThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_themeModeKey, _themeMode.index);
   }
 }
